d94fb1f74717338f6f223c064f699ddf71b0a12e
Averageasd <nhuynh9497@gmail.com>
2024-08-02 21:51:20 -0400

    added readme

--- FirstWebApp/obj/Debug/net6.0/FirstWebApp.AssemblyInfo.cs
+++ FirstWebApp/obj/Debug/net6.0/FirstWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+318dd4d296088663f9e69416df4680003e1997e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
